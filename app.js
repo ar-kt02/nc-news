42ee735e97f80ea647265c3459f53372529cb8cf
A Katz <akatz@Mac-mini.local>
2024-07-15 16:02:36 +0100

    3: GET /api

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics.controllers.js");
+const { getEndpoints } = require("./controllers/endpoints.controllers.js");
 const { serverErrors } = require("./error-handlers.js");
+
+app.get("/api", getEndpoints);
 
 app.get("/api/topics", getTopics);
 
