1bf6a01c417efa6661e2d93af1420d82d4aab5c0
A Katz <akatz@Mac-mini.local>
2024-07-17 18:44:21 +0100

    11: GET /api/articles (sorting queries)

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,8 +1,29 @@
 const db = require("../db/connection");
+const { sort } = require("../db/data/test-data/articles");
 const { checkArticleExists } = require("../utils");
 
-exports.fetchArticles = () => {
-   const sqlStr = `
+exports.fetchArticles = (sort_by = "created_at", order = "desc") => {
+   const validSortBys = [
+      "article_id",
+      "title",
+      "topic",
+      "author",
+      "body",
+      "created_at",
+      "votes",
+      "article_img_url",
+      "comment_count",
+   ];
+   const validOrders = ["asc", "desc"];
+
+   if (
+      !validSortBys.includes(sort_by) ||
+      !validOrders.includes(order.toLowerCase())
+   ) {
+      return Promise.reject({ status: 400, msg: "Invalid query" });
+   }
+
+   let sqlStr = `
    SELECT
    articles.author, 
    articles.title, 
@@ -14,8 +35,9 @@ exports.fetchArticles = () => {
    (SELECT COUNT(comment_id)::INT FROM comments WHERE comments.article_id = articles.article_id) AS comment_count
    FROM 
    articles
-   ORDER BY 
-   articles.created_at DESC`;
+   `;
+
+   sqlStr += ` ORDER BY ${sort_by} ${order.toLowerCase()}`;
 
    return db.query(sqlStr).then(({ rows }) => {
       return rows;
