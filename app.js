89ae54ec98695110b40e039b81a8c85681f697c2
A Katz <akatz@Mac-mini.local>
2024-08-05 10:25:46 +0100

    Fixed CORS setup

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-app.use(cors());
-
 const express = require("express");
-const app = express();
 const cors = require("cors");
+
+const app = express();
+app.use(cors());
 
 const apiRouter = require("./routes/api-router");
 const {
